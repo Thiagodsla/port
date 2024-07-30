609c4fcd8e0e81ff66935009bc444b0bad57bd07
thiago <thiagod11@hotmail.com>
2024-07-29 22:53:36 -0300

    wip(Port)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import * as React from 'react';
+// import * as React from 'react';
 import './App.css'
 import { Home } from './pages/Home'
 import { ChakraProvider } from '@chakra-ui/react'
